d95ecaa22406bca092e3a16e85d655e44694fb7f
Andrey Kruglik <isekaiguy@protonmail.com>
2022-09-17 17:24:20 +0300

    Added List component

---
+++ src/components/List/List.tsx
@@ -0,0 +1,12 @@
+import Card from "../Card/Card";
+import './List.scss';
+
+const List = () => {
+    return (
+        <div className='list'>
+            {/*<Card />*/}
+        </div>
+    )
+}
+
+export default List;
